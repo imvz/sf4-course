2c3f78fb8248748999eb5ac3769d62ba56e91141
vagrant <242851+imvz@users.noreply.github.com>
2018-06-11 11:11:04 +0200

    Tema 4

--- src/Controller/DeportesController.php
+++ src/Controller/DeportesController.php
@@ -1,8 +1,12 @@
 <?php
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-class DeportesController {
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DeportesController extends Controller {
    /**
     * @Route("/deportes")
     */
@@ -10,24 +14,29 @@ class DeportesController {
        return new Response('Mi página de deportes!');
    }
    
-    /**
-    * @Route("/deportes/{slug}")
-    */
-    public function mostrar($slug) {
-        return new Response(sprintf(
-           'Mi artículo en mi pagina de deportes: ruta %s',$slug));
-    }
+    // /**
+    // * @Route("/deportes/{slug}")
+    // */
+    // public function mostrar($slug) {
+    //     return new Response(sprintf(
+    //        'Mi artículo en mi pagina de deportes: ruta %s',$slug));
+    // }
 
     /**
     * @Route("/deportes/{seccion}/{pagina}", name="lista_paginas",
     *      requirements={"pagina"="\d+"},
     *      defaults={"seccion":"tenis"})
     */
-    public function lista($seccion,$pagina=1) {
-          return new Response(sprintf(
-           'Deportes seccion: seccion %s, listado de noticias página %s',
-           $seccion, $pagina));
-    }
+    // public function lista($seccion,$pagina=1) {
+    //     // Simulamos una base de datos de deportes
+    //     $deportes=["futbol", "tenis","rugby"];
+    //     // Si el deporte que buscamos no se encuentra lanzamos la
+    //     // excepcion 404 deporte no encontrado
+    //     if(!in_array($seccion,$deportes)) {
+    //         throw $this->createNotFoundException('Error 404 este deporte no está en nuestra Base de Datos');
+    //     }
+    //     return new Response(sprintf( 'Deportes seccion: seccion %s, listado de noticias página %s', $seccion, $pagina));
+    // }
 
     /**
     * @Route("/deportes/{seccion}/{slug} ",
@@ -52,12 +61,12 @@ class DeportesController {
     *     }
     * )
     */
-    public function rutaAvanzadaListado($_idioma,$fecha, $seccion, $equipo, $pagina) {
-       return new Response(sprintf(
-           'Listado de noticias  en idioma=%s,
-            fecha=%s,deporte=%s,equipo=%s, página=%s ',
-           $_idioma, $fecha, $seccion, $equipo, $pagina));
-    }
+    // public function rutaAvanzadaListado($_idioma,$fecha, $seccion, $equipo, $pagina) {
+    //    return new Response(sprintf(
+    //        'Listado de noticias  en idioma=%s,
+    //         fecha=%s,deporte=%s,equipo=%s, página=%s ',
+    //        $_idioma, $fecha, $seccion, $equipo, $pagina));
+    // }
 
     /**
     * @Route(
@@ -70,11 +79,39 @@ class DeportesController {
     *     }
     * )
     */
+    
     public function rutaAvanzada($_idioma,$fecha, $seccion, $equipo, $slug) {
+        // Simulamos una base de datos de equipos o personas
+        $deportes=["valencia", "barcelona","federer", "rafa-nadal"];
+
+       // Si el equipo o persona que buscamos no se encuentra redirigimos
+       // al usuario a la página de inicio
+
+       if(!in_array($equipo,$deportes)) {
+          return $this->redirectToRoute('inicio');
+       }
        return new Response(sprintf(
            'Mi noticia en idioma=%s,
             fecha=%s,deporte=%s,equipo=%s, noticia=%s ',
            $_idioma, $fecha, $seccion, $equipo, $slug));
     }
 
+    /**
+    * @Route("/deportes/usuario", name="usuario" )
+    */
+    public function sesionUsuario(Request $request) {
+        $usuario_get=$request->query->get('nombre');
+        $session = $request->getSession();
+        $session->set('nombre', $usuario_get);
+        return $this->redirectToRoute('usuario_session',array('nombre'=>$usuario_get));
+    }
+    /**
+    * @Route("/deportes/usuario/{nombre}", name="usuario_session" )
+    */
+    public function paginaUsuario() {
+       $session=new Session();
+       $usuario=$session->get('nombre');
+       return new Response(sprintf('Sesión iniciada con el atributo nombre: %s', $usuario
+       ));
+    }
 }
