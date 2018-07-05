9dc61326b08fe056e850e1d49054f45181b04ad4
vagrant <242851+imvz@users.noreply.github.com>
2018-07-05 14:06:54 +0200

    Tema 8

---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Usuario;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+class UsuarioController extends Controller
+
+{
+
+    /**
+    * @Route("/admin/login", name="login_seguro" )
+    */
+
+    public function loginUsuario​(Request $request, AuthenticationUtils $authUtils)
+    
+    {
+// Capturamos el error de autenticación
+        $error = $authUtils->getLastAuthenticationError();
+// Último nombre de usuario autenticado
+        $lastUsername = $authUtils->getLastUsername();
+        
+        return $this->render( 'Security/login.html.twig' , [
+            'last_username'=> $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    /**
+    * @Route("/admin/nuevousuario", name="usuariobd")
+    */
+    public function nuevoUsuarioBd​ ()
+    {
+        $em= $this->getDoctrine()->getManager();
+        $usuario= new Usuario();
+        $usuario->setEmail("jose@imaginaformacion.com");
+        $usuario->setUsername("jose");
+        $password = $this->get('security.password_encoder' )->encodePassword($usuario, "imaginapass" );
+        $usuario->setPassword($password);
+        $em->persist($usuario);
+        $em->flush();
+        return new Response( "Usuario guardado!" );
+    }
+}
